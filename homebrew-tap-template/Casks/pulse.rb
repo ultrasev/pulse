7b377dc5b5c769564169946a8faff953d7f8ca2e
ultrasev <i@ultrasev.com>
2026-01-01 01:34:00 +0800

    chore(homebrew-tap-template/Casks/pulse.rb): update homebrew cask version and add postflight quarantine removal

--- homebrew-tap-template/Casks/pulse.rb
+++ homebrew-tap-template/Casks/pulse.rb
@@ -1,5 +1,5 @@
 cask "pulse" do
-  version "0.0.2"
+  version "0.0.3"
   sha256 :no_check  # 首次发布后用实际 sha256 替换
 
   # Apple Silicon
@@ -14,6 +14,12 @@ cask "pulse" do
   homepage "https://github.com/ultrasev/pulse"
 
   app "Pulse.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-d", "com.apple.quarantine", "#{appdir}/Pulse.app"],
+                   sudo: false
+  end
 
   zap trash: [
     "~/Library/Application Support/com.pulse.dev",
